8a16907965995ac56613236cd78b473500aa6215
Anita <a.nita@hotmail.co.uk>
2016-09-10 16:14:02 +0100

    added fetcher helper and get imgSrc see https://github.com/heron2014/e-today/issues/4

--- tests/_runner.js
+++ tests/_runner.js
@@ -1,2 +1,3 @@
 require('./server.test.js');
 require('./collection.test.js');
+require('./fetcher.test.js');
--- tests/collection.test.js
+++ tests/collection.test.js
@@ -3,12 +3,25 @@
 const test = require('tape');
 const server = require('../lib/server.js');
 const Collections = require('../lib/collection');
+const Cheerio = require('cheerio');
 
 test('Access collections/1 endpoint and receive status code 200', (t) => {
   t.plan(1);
 
   server.inject({url: '/collections/1', method: 'GET'}, response => {
     t.equals(response.statusCode, 200, 'Status code is 200');
+    t.end();
+  });
+});
+
+test('Attempt to access /collections/50 content and display message: No more daily images', (t) => {
+  t.plan(2);
+
+  server.inject({url: '/collections/50', method: 'GET'}, (response) => {
+    let $ = Cheerio.load(response.payload);
+
+    t.equals($('.msg').text(), 'No more daily images', 'Message shows on the page');
+    t.equals(response.statusCode, 200, 'Status code 200');
     t.end();
   });
 });
